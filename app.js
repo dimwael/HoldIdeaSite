420675927ddbcf5ed38a36a3f7d3b3f219626a75
dimwael <32783348+dimwael@users.noreply.github.com>
2018-06-17 17:10:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const path = require('path');
+const exphbs  = require('express-handlebars');
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//https://pure-falls-56606.herokuapp.com/
+const app = express();
+// Load routers
+const ideas = require('./routers/ideas');
+const users = require('./routers/users');
+
+// Passport Config
+require('./config/passport')(passport)
+// DB config
+const db = require('./config/database');
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+console.log('before');
+// Connect to mongoose
+mongoose.connect(db.mongoURI , {
+  useMongoClient: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+console.log('after');
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Method override middleware
+app.use(methodOverride('_method'));
+
+// Express session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+
+//Global Variables
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null ;
+
+  next();
+});
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+//user routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
+});
